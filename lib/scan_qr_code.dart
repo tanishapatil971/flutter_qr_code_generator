12bca1f2d4231fe78c284f59c561e114209462a5
Tanisha Patil <tanishapatil971@gmail.com>
2026-01-02 12:36:35 +0530

    Initial commit - Flutter QR Code Generator

---
+++ lib/scan_qr_code.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+
+class ScanQRCode extends StatefulWidget {
+  const ScanQRCode({super.key});
+
+  @override
+  State<ScanQRCode> createState() => _ScanQRCodeState();
+}
+
+class _ScanQRCodeState extends State<ScanQRCode> {
+  String qrResult = 'Scanned Data will appear here';
+  Future<void> scanQR()async{
+    try{
+      final qrCode= await FlutterBarcodeScanner.scanBarcode('#ff6666', 'Cancel', true, ScanMode.QR);
+      if(!mounted)return;
+      setState(() {
+        this.qrResult = qrCode.toString();
+      });
+
+    }on PlatformException{
+      qrResult = 'Fail to read QR code';
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('QR Code Scanner'),),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(height: 30,),
+            Text('$qrResult', style: TextStyle(color: Colors.black), ),
+            SizedBox(height: 30,),
+            ElevatedButton(onPressed: scanQR, child: Text('Scan Code'))
+          ],
+        ),
+      ),
+    );
+  }
+}
